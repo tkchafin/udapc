22151926962c2421e9a9897d30e47bddea9ac0b8
tkchafin <tylerkchafin@gmail.com>
2023-03-26 23:09:57 +0100

    updated udapc code with changes from phylo_impute; implemented read=write VCF

--- test_dapc.py
+++ test_dapc.py
@@ -7,44 +7,45 @@ import pandas as pd
 import scipy.stats as stats
 
 # Custom module imports
-from read_input.read_input import GenotypeData
-from read_input.impute import *
+import udapc
+from udapc.read_input.read_input import GenotypeData
+from udapc.simple_imputers.simple_imputers import ImputeAlleleFreq
+from udapc.dim_reduction.embed import runPCA 
+from udapc.discriminant_analysis.discriminant_analysis import * 
 
-from sklearn_genetic.space import Continuous, Categorical, Integer
+# from dim_reduction.dim_reduction import DimReduction
+# from dim_reduction.embed import *
 
-from dim_reduction.dim_reduction import DimReduction
-from dim_reduction.embed import *
+# from clustering.clustering import *
 
-from clustering.clustering import *
-
-from discriminant_analysis.discriminant_analysis import DiscriminantAnalysis
+# from discriminant_analysis.discriminant_analysis import DiscriminantAnalysis
 
 def main():
 	"""[Class instantiations and main package body]
 	"""
 
-	args = get_arguments()
-
 	data = GenotypeData(
-		filename="example_data/phylip_files/test2.phy", 
-		filetype="phylip", 
-		popmapfile="example_data/popmaps/test2.popmap"
+		filename="example_data/mah_thin.vcf.gz", 
+		filetype="vcf", 
+		popmapfile="example_data/mah.popmap"
 	)
+	print(data.genotypes012_array)
 
 	imp = ImputeAlleleFreq(
 		genotype_data = data,
-		pops=data.pops
+		by_populations=True
 	)
-	print(imp.imputed)
+	print(imp.imputed.genotypes012_array)
+
 	#pca.coords dimensions: samples X pcs
 	pca = runPCA(
-		gt=imp.imputed, 
+		gt=imp.imputed.genotypes012_array, 
 		pops=data.pops, 
 		sampleids=data.samples,
 		prefix="test", 
 		scaler="patterson",
 		plot_cumvar=False, 
-		keep_pcs=100
+		keep_pcs=89
 	)
 	pca.plot()
 	
@@ -62,78 +63,7 @@ def main():
 	dapc.plot(axis1=1, axis2=2)
 	dapc.plot(axis1=2, axis2=3)
 	
-	
-def get_arguments():
-	"""[Parse command-line arguments. Imported with argparse]
 
-	Returns:
-		[argparse object]: [contains command-line arguments; accessed as method]
-	"""
-
-	parser = argparse.ArgumentParser(description="Convert VCF file to BGC format (with genotype uncertainties). Currently only handles three populations maximum (P1, P2, and Admixed).", add_help=False)
-
-	required_args = parser.add_argument_group("Required arguments")
-	filetype_args = parser.add_argument_group("File type arguments (choose only one)")
-	structure_args = parser.add_argument_group("Structure file arguments")
-	optional_args = parser.add_argument_group("Optional arguments")
-
-	# File Type arguments
-	filetype_args.add_argument("-s", "--str",
-								type=str,
-								required=False,
-								help="Input structure file")
-	filetype_args.add_argument("-p", "--phylip",
-								type=str,
-								required=False,
-								help="Input phylip file")
-	
-	filetype_args.add_argument("-t", "--treefile",
-								type=str,
-								required=False,
-								default=None,
-								help="Newick-formatted treefile")
-
-	# Structure Arguments
-	structure_args.add_argument("--onerow_perind",
-								default=False,
-								action="store_true",
-								help="Toggles on one row per individual option in structure file")
-	structure_args.add_argument("--pop_ids",
-								default=False,
-								required=False,
-								action="store_true",
-								help="Toggles on population ID column (2nd col) in structure file")
-	
-	
-	## Optional Arguments
-	optional_args.add_argument("-m", "--popmap",
-								type=str,
-								required=False,
-								default=None,
-								help="Two-column tab-separated population map file: inds\tpops. No header line")
-	optional_args.add_argument("--prefix",
-								type=str,
-								required=False,
-								default="output",
-								help="Prefix for output files")
-
-	optional_args.add_argument("--resume_imputed",
-								type=str,
-								required=False,
-								help="Read in imputed data from a file instead of doing the imputation")						
-	# Add help menu							
-	optional_args.add_argument("-h", "--help",
-								action="help",
-								help="Displays this help menu")
-
-	# If no command-line arguments are called then exit and call help menu.
-	if len(sys.argv)==1:
-		print("\nExiting because no command-line options were called.\n")
-		parser.print_help(sys.stderr)
-		sys.exit(1)
-
-	args = parser.parse_args()
-	return args
 
 if __name__ == "__main__":
 	main()
